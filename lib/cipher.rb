24be1f6273936abad5eb561895511955b1828a2c
Alex Risberg <alex.risberg@gmail.com>
2015-04-13 11:11:16 -0700

    encription

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,3 +1,20 @@
 class Cipher
-
+        def encrypt (message, shift)
+            alph = ('a'..'z').to_a
+            arr = []
+            finalarr = []
+            endarr = []
+            arr = message.gsub(/\s+/, "").split('')
+            arr.each do |i|
+                finalarr.push(alph.index(i))
+            end
+            finalarr.each do |x|
+                endarr.push(alph[x + shift])
+            end
+            return endarr
+        end
 end
+
+new = Cipher.new
+new.encrypt("cat", 2)
+
